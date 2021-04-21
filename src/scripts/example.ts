32bdd9c86c376624740d78c751ff5914c3ffa66b
Shouvojit Sarker <sxsarker@agiledigital.com.au>
2021-04-21 12:34:11 +1000

    ADE-671 = Remove explicit yargs type

--- src/scripts/example.ts
+++ src/scripts/example.ts
@@ -1,11 +1,10 @@
 import { RootCommand } from '..';
-import { Argv } from 'yargs';
 
 export default ({ command }: RootCommand) =>
   command(
     'name',
     'description',
-    (yargs: Argv) => yargs,
+    (yargs) => yargs,
     (args) => {
       // Add your code here
     }

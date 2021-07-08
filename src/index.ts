22b207ec23ab412574db72573d85a52f39a9a704
Daniel Nixon <dan.nixon@gmail.com>
2021-07-08 14:12:58 +1000

    ADE-799 = Beef up eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable functional/no-expression-statement */
 import { readdirSync } from 'fs';
 import { join } from 'path';
 import yargs from 'yargs';
@@ -9,7 +12,7 @@ import yargs from 'yargs';
 export type RootCommand = typeof rootCommand;
 
 // Only finds scripts in top level
-const directorySearch = (localDirectory: string) =>
+const directorySearch = (localDirectory: string): readonly string[] =>
   readdirSync(join(__dirname, localDirectory), { withFileTypes: true })
     .map(({ name }) => name)
     .filter((value) => value.endsWith('.js'))
@@ -25,6 +28,7 @@ const rootCommand = yargs;
 
 // Allows to configure handlers (any .js file in the scripts directory) with arguments (rootCommand in this case) at runtime.
 // This means the end users of this tool won't have to touch this file, they just have to add their scripts in the scripts folder.
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 directorySearch('scripts').map((path) => require(path)(rootCommand));
 
 rootCommand.demandCommand().strict().help().argv;
--- src/scripts/example.ts
+++ src/scripts/example.ts
@@ -1,12 +1,14 @@
+import { Argv } from 'yargs';
 import { RootCommand } from '..';
 
-export default ({ command }: RootCommand) =>
+export default ({ command }: RootCommand): Argv<unknown> =>
   command(
     'name',
     'description',
     (yargs) => yargs,
     (args) => {
       // Add your code here
+      // eslint-disable-next-line functional/no-expression-statement
       console.info(args);
     }
   );

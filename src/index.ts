a863290f64651549cfecf44c840ba362e171effb
ohwalters <ohwalters@agiledigital.com.au>
2022-10-12 14:29:13 +1100

    IE-40 = index.ts: added type coverage for the new require

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,20 @@
 /* eslint-disable functional/no-expression-statement */
 import { readdirSync } from 'fs';
 import { join } from 'path';
-import yargs from 'yargs';
+import yargs, { Argv } from 'yargs';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, functional/prefer-type-literal
+    interface Require {
+      <T>(id: string): T;
+    }
+  }
+}
+
+// eslint-disable-next-line functional/no-return-void
+type DefaultFunction = (rootCommand: RootCommand) => Argv<unknown>;
 
 /**
  * Dynamic type for global arguments. This needs to be it's own as we use a
@@ -30,10 +43,11 @@ const rootCommand = yargs;
 // This means the end users of this tool won't have to touch this file, they just have to add their scripts in the scripts folder.
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 directorySearch('scripts').map((path) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const command = require(path);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  const command = require<
+    { readonly default: DefaultFunction } | DefaultFunction
+  >(path);
+
   return (typeof command === 'object' ? command.default : command)(rootCommand);
 });
 
-rootCommand.demandCommand().strict().help().argv;
+void rootCommand.demandCommand().strict().help().argv;
